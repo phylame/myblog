5a9e8943c54dfbfba9c4b19baba2843f194791ea
wp <phylame@163.com>
2018-01-09 23:28:51 +0800

    work for post list

---
+++ src/main/kotlin/pw/phylame/myblog/Config.kt
@@ -0,0 +1,8 @@
+package pw.phylame.myblog
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+
+@Configuration
+class WebMvcConfig : WebMvcConfigurerAdapter() {
+}
--- src/main/kotlin/pw/phylame/myblog/web/PostController.kt
+++ src/main/kotlin/pw/phylame/myblog/web/PostController.kt
@@ -6,6 +6,7 @@ import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import pw.phylame.myblog.data.ArticleRepository
+import pw.phylame.myblog.data.CategoryRepository
 import javax.servlet.http.HttpServletRequest
 
 @Controller
@@ -17,9 +18,13 @@ class PostController {
     @field:Autowired
     lateinit var articleRepository: ArticleRepository
 
+    @field:Autowired
+    lateinit var categoryRepository: CategoryRepository
+
     @GetMapping(value = ["", "/"])
     fun listBlogs(model: Model): String {
         model.addAttribute("posts", articleRepository.findAll())
-        return "blog"
+        model.addAttribute("categories", categoryRepository.findAll())
+        return "post/list"
     }
 }

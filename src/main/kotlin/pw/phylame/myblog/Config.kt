f77c62ffd0aaec702f0ff5e47f7ad46c1ab2bb61
wp <phylame@163.com>
2018-01-19 15:34:25 +0800

    add cors for dev profile

--- src/main/kotlin/pw/phylame/myblog/Config.kt
+++ src/main/kotlin/pw/phylame/myblog/Config.kt
@@ -1,7 +1,29 @@
 package pw.phylame.myblog
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+
 
 @Configuration
 class WebMvcConfig : WebMvcConfigurerAdapter()
+
+@Configuration
+class GlobalRepositoryRestConfigurer : RepositoryRestConfigurerAdapter() {
+    @field:Autowired
+    private lateinit var env: Environment
+
+    override fun configureRepositoryRestConfiguration(config: RepositoryRestConfiguration) {
+        if ("dev" in env.activeProfiles) {
+            config.corsRegistry
+                    .addMapping("/api/*")
+                    .allowedOrigins("*")
+                    .allowedHeaders("*")
+                    .allowedMethods("*")
+        }
+    }
+
+}
